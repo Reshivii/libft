9fd8ae0746a85eadde60d7518d6f6a31c3b12435
aburnott <aburnott@student.s19.be>
2022-10-11 18:32:34 +0200

    All done ! (No free in split)

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: aburnott <aburnott@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/05 14:32:50 by aburnott          #+#    #+#             */
-/*   Updated: 2022/10/11 15:13:04 by aburnott         ###   ########.fr       */
+/*   Updated: 2022/10/11 18:09:59 by aburnott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,12 @@ int	ft_atoi(const char *str)
 	int				i;
 	int				sign;
 	unsigned long	res;
+	int				temp;
 
 	i = 0;
 	sign = 1;
 	res = 0;
+	temp = 0;
 	while (str[i] == '\t' || str[i] == '\n' || str[i] == '\v'
 		|| str[i] == '\f' || str[i] == '\r' || str[i] == ' ')
 		i++;
@@ -30,11 +32,11 @@ int	ft_atoi(const char *str)
 			sign *= -1;
 		i++;
 	}
-	while (str[i] >= '0' && str[i] <= '9')
+	while (str[i] >= '0' && str[i] <= '9' && ++temp)
 		res = res * 10 + str[i++] - 48;
-	if (res >= __LONG_MAX__ && sign == 1)
+	if ((res >= 9223372036854775807 || temp >= 20) && sign == 1)
 		return (-1);
-	else if (res > __LONG_MAX__ && sign == -1)
+	else if ((res > 9223372036854775807 || temp >= 20) && sign == -1)
 		return (0);
 	return (sign * res);
 }
--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: aburnott <aburnott@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 15:39:14 by aburnott          #+#    #+#             */
-/*   Updated: 2022/10/11 15:40:01 by aburnott         ###   ########.fr       */
+/*   Updated: 2022/10/11 18:29:29 by aburnott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,28 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	
+	t_list	*new;
+	t_list	*tab;
+	t_list	*tmp;
+
+	if (!lst || !f)
+		return (0);
+	new = ft_lstnew(f(lst->content));
+	if (!new)
+		return (0);
+	tab = new;
+	lst = lst->next;
+	while (lst)
+	{
+		tmp = ft_lstnew(f(lst->content));
+		if (!tmp)
+		{
+			ft_lstclear(&new, del);
+			return (0);
+		}
+		tab->next = tmp;
+		tab = tmp;
+		lst = lst->next;
+	}
+	return (new);
 }
